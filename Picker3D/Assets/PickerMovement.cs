6d8592464c40ce6461b6e3d9478c8c321d516029
AbdullahCemalKervankiran <abdullahcemal12@gmail.com>
2022-05-25 18:53:27 +0300

    Simple Picker and Road added

---
+++ Picker3D/Assets/PickerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class PickerMovement : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        _rigidbody.velocity += 0.3f*Vector3.forward;
+    }
+}
