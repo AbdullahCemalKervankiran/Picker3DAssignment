ff4e9667221a0e7371ae6045d2043ba28b9a5ec6
AbdullahCemalKervankiran <abdullahcemal12@gmail.com>
2022-05-26 15:29:46 +0300

    PickerMovement fixed, PoolText set

--- Picker3D/Assets/PickerMovement.cs
+++ Picker3D/Assets/PickerMovement.cs
@@ -6,19 +6,21 @@ using UnityEngine;
 
 public class PickerMovement : MonoBehaviour
 {
+    public Transform stopPoint;
+    
     private Rigidbody _rigidbody;
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
+    private void Start()
     {
         Move();
     }
 
     private void Move()
     {
-        _rigidbody.DOMoveZ(40, 10).SetUpdate(UpdateType.Fixed); //_rigidbody.velocity += 0.3f*Vector3.forward;
+        _rigidbody.DOMoveZ(stopPoint.position.z, 5f).SetEase(Ease.Linear).SetUpdate(UpdateType.Fixed); //_rigidbody.velocity += 0.3f*Vector3.forward;
     }
 }
---
+++ Picker3D/Assets/PoolCounter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PoolCounter : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro ballCountText;
+    private int _ballCount;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            other.tag = "Untagged";
+            _ballCount++;
+            ballCountText.text = _ballCount + "/20";
+        }
+    }
+}
